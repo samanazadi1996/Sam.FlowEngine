8bd0f20a876ce59024b8938bfa7a9ed6f5369aa9
samanazadi1996 <samanazadi1996@gmail.com>
2026-01-31 12:34:38 +0330

    2 files modified and 3 files added
    Modified : ../FrontEnd/FlowEngine/src/app/modules/main/main-module.ts
    Added : ../FrontEnd/FlowEngine/src/app/modules/main/pages/projects/create-project/create-project.html
    Added : ../FrontEnd/FlowEngine/src/app/modules/main/pages/projects/create-project/create-project.scss
    Added : ../FrontEnd/FlowEngine/src/app/modules/main/pages/projects/create-project/create-project.ts
    Modified : ../FrontEnd/FlowEngine/src/app/modules/main/pages/projects/project-detail/project-detail.html

--- FrontEnd/FlowEngine/src/app/modules/main/main-module.ts
+++ FrontEnd/FlowEngine/src/app/modules/main/main-module.ts
@@ -5,16 +5,21 @@ import { MainRoutingModule } from './main-routing-module';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Projects } from './pages/projects/projects';
 import { ProjectDetail } from './pages/projects/project-detail/project-detail';
+import { CreateProject } from './pages/projects/create-project/create-project';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     Dashboard,
     Projects,
-    ProjectDetail
+    ProjectDetail,
+    CreateProject
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    FormsModule
+
   ]
 })
 export class MainModule { }
---
+++ FrontEnd/FlowEngine/src/app/modules/main/pages/projects/create-project/create-project.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-project',
+  standalone: false,
+  templateUrl: './create-project.html',
+  styleUrl: './create-project.scss',
+})
+export class CreateProject {
+
+}
